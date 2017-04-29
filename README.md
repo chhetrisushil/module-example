ca8317d659c4b80a42fd13a8058b5bbca635a919
Chhetri <chhes@amazon.com>
2017-04-29 12:46:43 +0530

    Creating a repo for module example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# module-example
+# jifflenow-test
+
+### View the output
+* Run `npm install`
+* Run `npm start`
+* Open `http://localhost:8001` in **safari**
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+/*
+ * main.js
+ * Copyright (C) 2017 Sushil Chhetri <chhetrisushil@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+/*jshint esnext: true*/
+import {
+  quizView
+}
+from './views.js';
+import {
+  getQuestions
+}
+from './datasource.js';
+
+async function init() {
+  let questions = await getQuestions();
+
+  quizView(questions);
+}
+
+init();
